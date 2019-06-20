b715d0502d3dee2328e97d228ea67739811f6176
ottowei <ottowei@suntyam.com>
2019-06-20 18:01:36 +0800

    2019062001 Hardware Debug ==================== 1. Use HH's code to enable port 0 IO. 2. Measure port 0. Results: 1. Port 0 set to output OK. 2. Port 0 pull low OK.

--- Examples/Example_PCA9555/Example_PCA9555.ino
+++ Examples/Example_PCA9555/Example_PCA9555.ino
@@ -5,33 +5,48 @@
 #endif
 #include <Wire.h>
 char incomingByte = 0;
-#define U14PCA9555_ADDRESS (0x20)
-#define U15PCA9555_ADDRESS (0x21)
+#define U14PCA9555_ADDRESS 0x20
+#define U15PCA9555_ADDRESS 0x21
 #define DATA_SIZE 8
 
 void setup() {
   Serial.begin(9600);
-}
-
-void loop() {
   Serial.println("<Aduino is ready>");
   Serial.println("U14 Address is");
   Serial.println(U14PCA9555_ADDRESS);//32
-  Wire.beginTransmission(U14PCA9555_ADDRESS);
+  Wire.begin();
+  Wire.beginTransmission(0x20);
   Wire.write(0x06);  // pointer
-  Wire.write(0x0F);  // DDR Port0 bits 0-3 input 4-7 output 
+  Wire.write(0x00);  // DDR Port0 all output
   Wire.write(0x00);  // DDR Port1 all output
   Wire.endTransmission(); 
-  Serial.println("U14PCA9555_ADDRESS, 0x06,0x0F,0x00"); 
-  delay(3000);
   Serial.println("U15 Address is");
   Serial.println(U15PCA9555_ADDRESS);//33
-  Wire.beginTransmission(U15PCA9555_ADDRESS);
+  Wire.beginTransmission(0x21);
   Wire.write(0x06);  // pointer
-  Wire.write(0x0F);  // DDR Port0 bits 0-3 input 4-7 output 
+  Wire.write(0x00);  // DDR Port0 all output
   Wire.write(0x00);  // DDR Port1 all output
   Wire.endTransmission(); 
-  Serial.println("U15PCA9555_ADDRESS, 0x06,0x0F,0x00"); 
-   delay(2000);
+
+}
+
+void loop() {
+
+  Wire.beginTransmission(0x20);
+  Wire.write(0x02);  // Output Port 0
+  Wire.write(0x00);  // 0xFF = 5V //DDR Port0 bits 0-3 input 4-7 output
+  // Wire.write(0x00);  // DDR Port1 all output
+  Wire.endTransmission();
+  Serial.println("U14PCA9555_ADDRESS, 0x02,0x00,0x00");
+  delay(3000);
+  // Serial.println("U15 Address is");
+  // Serial.println(U15PCA9555_ADDRESS);//33
+  Wire.beginTransmission(0x21);
+  Wire.write(0x02);  // pointer
+  Wire.write(0x00);  // 0xFF = 5V  DDR Port0 bits 0-3 input 4-7 output
+  // Wire.write(0x00);  // DDR Port1 all output
+  Wire.endTransmission();
+  Serial.println("U15PCA9555_ADDRESS, 0x02,0x00,0x00");
+  delay(2000);
  
 }
